93327c0176cdee4549f7e7b70ca8bb8b756294c4
KimYeJi <2214yj@naver.com>
2019-11-27 20:01:53 +0900

    create&show&modify&delete comment but no use postId

--- routes/comments.js
+++ routes/comments.js
@@ -1,32 +1,82 @@
 const express = require('express');
-const { Comment , User  } = require('../models');
+const { Comment , User ,Post } = require('../models');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
-const moment = require('moment');
 const Op = sequelize.Op;
 
+//isLoggedIn,
+router.get('/',isLoggedIn, async (req,res,next) => { //comment 보여주기
+    //postId 일단 빼고
+    try{
+        // if(req.params.postId){
+            const comment = await Comment.findAll({
+                // where : {postId : req.params.postId }
+            });
+            res.json(comment);
+            return;
+        // }
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
 
-router.post('/:postId',isLoggedIn, async (req,res,next) => { //comment 보여주고 생성
+router.post('/create',isLoggedIn, async (req,res,next) => { //comment 생성
     //postId 일단 빼고
     const { content } = req.body;
     try{
-        if(!content){
-            const newComment = await Comment.create({
-                content,  
-                postId : req.params.postId, //댓글 게시글에따라 보여줌
-                userId : req.user.id,
-            });
-            const comments = await Comment.findAll();
-            //완성 후 조건주기{where : {postId : postId}}
-            res.json(comments);
+        if(!content){ //content가 null여면 true
+        return;
         }
-            const comments = await Comment.findAll();
-            //완성 후 조건주기{where : {postId : postId}}
-            res.json(comments);
- 
-    return;
+        else{
+                const newComment = await Comment.create({
+                    content : content,  
+                    //postId : req.params.postId, //댓글 게시글에따라 보여줌
+                    nick : "hi"
+            });
+        }
+        return res.json({
+            res : true,
+            msg : '댓글 생성 완료'
+        });
+    
     } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+
+router.get('/delete/:commentId',isLoggedIn,async(req,res,next)=>{
+    const commentId = req.params.commentId;
+    try{
+        Comment.destroy({
+            where: { id: commentId }
+        });
+        return res.json({
+            res : true,
+            msg : '댓글 삭제 완료'
+        });
+    }catch(err){
+        console.error(err);
+        next(err);
+    }
+});
+
+router.post('/modify/:commentId',isLoggedIn,async(req,res,next)=>{
+    const commentId = req.params.commentId;
+    const { content } = req.body;
+    try{
+        Comment.update({
+            content : content,
+        },{
+            where : {id: commentId}
+        });
+        return res.json({
+            res : true,
+            msg : '댓글 수정 완료'
+        });
+    }catch(err){
         console.error(err);
         next(err);
     }
