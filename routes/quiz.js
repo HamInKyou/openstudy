b9ff897663b034781525a28dbfbbb4e7eb28dd8a
yonghuKim <dydgndlaek97@gmail.com>
2019-12-04 02:14:20 +0900

    퀴즈 등록 post-quiz (이미지/정답 첨부 미구현)

--- routes/quiz.js
+++ routes/quiz.js
@@ -14,17 +14,19 @@ router.post('/create', isLoggedIn, async (req, res, next) => {
             ownerAnswerId,
             boardId,             
         });
-        if(ownerAnswerId){
+        if(ownerAnswerId != 0){
             const ownerAnswer = await Answer.findOne({
                 where: {id : ownerAnswerId}
             });
             quiz.addAnswer(ownerAnswer);
+        } else {
+            
         }
         const result = {
-            res : "success",
+            msg : "퀴즈가 등록 돼었습니다.",
             quizId : quiz.id
-        }; // 전페이지로 리다이렉트 시키는걸로 바꾸기
-        res.json(result);
+        };
+        return res.json(result);
     } catch(err){
         console.error(err);
         next(err);
