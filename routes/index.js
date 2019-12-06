f1f88f6073f2e0cfe62ea266540a5238b5eec85b
yejin <yejin013@naver.com>
2019-12-06 21:29:15 +0900

    study-post-list

--- routes/index.js
+++ routes/index.js
@@ -204,6 +204,15 @@ router.get('/mystudy-list', async (req, res, next) => {
 
 router.get('/study-post-list/:boardId', async (req, res, next) => {
   try {
+    const boardId = req.params.boardId;
+    const exBoard = await Board.findOne({include : {
+      model : Study,
+      attributes : ['name', 'info'],
+      raw : true
+    }, 
+    where : {
+      id : boardId
+    }});
     const exPosts = await Post.findAll({
       include : {
         model : User,
@@ -220,7 +229,7 @@ router.get('/study-post-list/:boardId', async (req, res, next) => {
       },
     });
     res.render('study-post-list', {
-      posts: JSON.stringify(exPosts)
+      board : JSON.stringify(exBoard), posts: JSON.stringify(exPosts)
     });
   } catch (err) {
     console.error(err);
