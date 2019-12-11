21de45a7ca2e10ea22bd3c543cb825b4bbfcd09d
yonghuKim <dydgndlaek97@gmail.com>
2019-12-12 01:03:05 +0900

    delete answe.name

--- routes/answer.js
+++ routes/answer.js
@@ -6,10 +6,9 @@ const { isLoggedIn } = require('./middlewares');
 //비동기로 사용
 // 처음 문제 생성할 때 정답 등록하는 용도
 router.post('/create' , isLoggedIn,  async (req, res, next) => {
-    const {name, content, url} = req.body;
+    const {content, url} = req.body;
     try{
         const answer = await Answer.create({
-            name,
             content,
             url,
             userId : req.user.id,
@@ -26,10 +25,9 @@ router.post('/create' , isLoggedIn,  async (req, res, next) => {
 });
 //이미 존재하는 퀴즈에 정답 제출하는 용도
 router.post('/submit', isLoggedIn, async (req, res, next) => {
-    const {name, content, url, quizId } = req.body;
+    const {content, url, quizId } = req.body;
     try{
         const answer = await Answer.create({
-            name,
             content,
             url,
             userId : req.user.id,
